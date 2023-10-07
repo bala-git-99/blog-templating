2c16a1a735d4cfd0af8fd5c31683fb98eb8ca84d
bala <balagrt1099@gmail.com>
2023-10-07 17:09:25 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+import requests
+
+response = requests.get(url="https://api.npoint.io/df923a4a07ef17cbdc3d")
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", posts=response.json())
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    get_h1 = f"Contact Me"
+    post_h1 = f"Successfully sent your message"
+    if request.method == 'GET':
+        return render_template("contactv2.html", h1=get_h1)
+    else:
+        return render_template("contactv2.html", h1=post_h1)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return render_template("post.html", post=response.json()[post_id - 1])
+
+
+@app.route('/form-entry', methods=['POST'])
+def receive_data():
+    name = request.form["name"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+    message = request.form["message"]
+    print(f"{name}\n{email}\n{phone}\n{message}")
+    return f"<h1>Successfully sent your message</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
